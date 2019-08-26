dc6e3007faed15318103c0ea4f8b992126d8fb8c
t.yoshizawa <t.yoshizawa@jc-21.ac.jp>
2019-08-26 19:36:38 +0900

    キャッシュを活用する版のダンプツールを作成、遅すぎて本番環境ではエラーが発生する

--- src/main/java/jp/ac/jc21/t/yoshizawa/admin/AnswerDumpAdminServlet.java
+++ src/main/java/jp/ac/jc21/t/yoshizawa/admin/AnswerDumpAdminServlet.java
@@ -3,6 +3,7 @@ package jp.ac.jc21.t.yoshizawa.admin;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
@@ -27,42 +28,62 @@ public class AnswerDumpAdminServlet extends HttpServlet {
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		final Logger log = Logger.getLogger(AnswerDumpAdminServlet.class.getName());
+		int count=0;
 
 		response.setContentType("text/csv; charset=Windows-31J");
 		PrintWriter out = response.getWriter();
 
 		List<AnswerSum> answerSumList = AnswerSum.loadAll();
 
-		UserService userService = UserServiceFactory.getUserService();
-
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm");
 
-		out.println("–âid,‰ğ“šÒ,‰ğ“š“ú,Œ±,–â,•ª–ì‡,•ª–ì,–âÚ×,İ–âid,o‘è‡,İ–â,³‰ğ,‰ğ“š,³Œë");
+		out.println("–âid,‰ğ“šÒ,‰ğ“š“ú,Œ±,–â,•ª–ì‡,•ª–ì,–âÚ×,"+
+										"İ–âid,o‘è‡,İ–â,³‰ğ,‰ğ“š,³Œë");
 
+		Date dateStart = new Date();
+		log.info(getServletName() + "[" + dateStart.toString() + "]START");
 		for (AnswerSum as : answerSumList) {
-			Toi toi = as.getRefToi().get();
-			Exam exam = toi.getExam();
+
+			String ansSumDump = as.getAnswerSumDumpCSV();
+			if(ansSumDump == null) {
+				Toi toi = as.getRefToi().get();
+				Exam exam = toi.getExam();
+				String s = as.getId() + "," 
+						+ as.getName() + "," 
+						+ sdf.format(as.getAnswered()) + "," 
+						+ exam.getName() + ","
+						+ toi.getNo() + "," 
+						+ toi.getRefGenre().get().getNo() + "," 
+						+ toi.getRefGenre().get().getName() + "," 
+						+ toi.getName() + ",";
+				as.setAnswerSumDumpCSV(s);
+				ansSumDump = s;
+				as.save();
+			}
+
 			Map<Integer, Answer> answerMap = as.getMapAnswer();
 			for (Integer key : answerMap.keySet()) {
 				Answer answer = answerMap.get(key);
-				Question question = answer.getRefQuestion().get();
 
-				out.print(as.getId());
-				out.print(",");
-				out.print(as.getName());
-				out.print(",");
-				out.print(sdf.format(as.getAnswered()));
-				out.print(",");
-				out.print(exam.getName());
-				out.print(",");
-				out.print(toi.getNo());
-				out.print(",");
-				out.print(toi.getRefGenre().get().getNo());
-				out.print(",");
-				out.print(toi.getRefGenre().get().getName());
-				out.print(",");
-				out.print(toi.getName());
-				out.print(",");
+				out.print(ansSumDump);
+				
+				String ansDump = answer.getAnswerDumpCSV();
+
+				if(ansDump==null) {
+					Question question = answer.getRefQuestion().get();
+					String s =answer.getId() + "," + 
+							key + "," + 
+							question.getName() + "," +
+							question.getAnswers() + "," +
+							answer.getAnswers() + "," +
+							(answer.isCorrect() ? 1 : 0);
+					ansDump=s;
+					answer.setAnswerDumpCSV(s);
+					answer.save();
+				}
+						
+				
+				/*
 				out.print(answer.getId());
 				out.print(",");
 				out.print(key);
@@ -74,11 +95,19 @@ public class AnswerDumpAdminServlet extends HttpServlet {
 				out.print(answer.getAnswers());
 				out.print(",");
 				out.print(answer.isCorrect() ? 1 : 0);
+*/
+				out.print(ansDump);
 
 				out.println();
 			}
 			out.flush();
+			log.info(getServletName() + "[" + new Date().toString() + "]" + count);
+			count++;
 		}
+		Date dateEnd = new Date();
+		log.info(getServletName() + "[" + dateEnd.toString() + "]END");
+		log.info(getServletName() + "[" +(dateEnd.getTime() - dateStart.getTime())+ "ms]END");
+		
 
 	}
 }
--- src/main/java/jp/ac/jc21/t/yoshizawa/admin/AnswerSumDumpAdminServlet.java
+++ src/main/java/jp/ac/jc21/t/yoshizawa/admin/AnswerSumDumpAdminServlet.java
@@ -3,16 +3,15 @@ package jp.ac.jc21.t.yoshizawa.admin;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
 
 import jp.ac.jc21.t.yoshizawa.objectify.*;
 
@@ -24,6 +23,8 @@ public class AnswerSumDumpAdminServlet extends HttpServlet {
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		final Logger log = Logger.getLogger(AnswerSumDumpAdminServlet.class.getName());
+
 		response.setContentType("text/csv; charset=Windows-31J");
 		PrintWriter out = response.getWriter();
 
@@ -32,29 +33,33 @@ public class AnswerSumDumpAdminServlet extends HttpServlet {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm");
 
 		out.println( "ID,âìöé“,âìöì˙,ééå±,ñ‚,ï™ñÏèá,ï™ñÏ,ñ‚è⁄ç◊,ê≥âêî,ñ‚ëËêî,ê≥âó¶" );
+		
+		Date dateStart = new Date();
+		log.info(getServletName() + "[" + dateStart.toString() + "]START");
 
 		for (AnswerSum as : answerSumList) {
 			if(as.getRefMember() != null) {
-				Toi toi = as.getRefToi().get();
-				Exam exam = toi.getExam();
-				float point=(100.0f * as.getNoOfSeikai() / as.getNoOfAnswer());
 
-				out.print( as.getId() );
-				out.print( "," );
-				out.print( as.getName() );
-				out.print( "," );
-				out.print( sdf.format(as.getAnswered()) );
-				out.print( "," );
-				out.print( exam.getName() );
-				out.print( "," );
-				out.print( toi.getNo()  );
-				out.print( "," );
-				out.print( toi.getRefGenre().get().getNo()  );
-				out.print( "," );
-				out.print( toi.getRefGenre().get().getName()  );
-				out.print( "," );
-				out.print( toi.getName()  );
-				out.print( "," );
+				String ansSumDump = as.getAnswerSumDumpCSV();
+				if(ansSumDump == null) {
+					Toi toi = as.getRefToi().get();
+					Exam exam = toi.getExam();
+					String s = as.getId() + "," 
+							+ as.getName() + "," 
+							+ sdf.format(as.getAnswered()) + "," 
+							+ exam.getName() + ","
+							+ toi.getNo() + "," 
+							+ toi.getRefGenre().get().getNo() + "," 
+							+ toi.getRefGenre().get().getName() + "," 
+							+ toi.getName() + ",";
+					as.setAnswerSumDumpCSV(s);
+					ansSumDump = s;
+					as.save();
+				}
+				float point=(100.0f * as.getNoOfSeikai() / as.getNoOfAnswer());
+				
+
+				out.print( ansSumDump );
 				out.print( as.getNoOfSeikai()  );
 				out.print( "," );
 				out.print( as.getNoOfAnswer()  );
@@ -64,10 +69,9 @@ public class AnswerSumDumpAdminServlet extends HttpServlet {
 				out.println(  );
 				out.flush();
 			}
-
 		}
-
-
-
+		Date dateEnd = new Date();
+		log.info(getServletName() + "[" + dateEnd.toString() + "]END");
+		log.info(getServletName() + "[" +(dateEnd.getTime() - dateStart.getTime())+ "ms]END");
 	}
 }
