fc0d5a94e60a6fe0fa0ac2b6c6f1cce161108499
t.yoshizawa <t.yoshizawa@jc-21.ac.jp>
2020-01-17 19:58:16 +0900

    エラーが取れないので放流

--- src/main/java/jp/ac/jc21/t/yoshizawa/admin/check/CheckToiMemberServlet.java
+++ src/main/java/jp/ac/jc21/t/yoshizawa/admin/check/CheckToiMemberServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.TreeMap;
 
@@ -28,66 +29,75 @@ import jp.ac.jc21.t.yoshizawa.objectify.Toi;
 @WebServlet("/admin/check/toiMember")
 public class CheckToiMemberServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CheckToiMemberServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public CheckToiMemberServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 //		response.setContentType("text/csv; charset=Windows-31J");
 //		PrintWriter out = response.getWriter();
-		
+
 		String keyString = request.getParameter("toiId");
-		Toi t =Toi.getById(Long.parseLong(keyString));
-		
+		Toi t = Toi.getById(Long.parseLong(keyString));
+
 		List<Ref<AnswerSum>> answerSumRefList = t.getAnswerSumRefList();
-		
+
 		ArrayList<String[]> list = new ArrayList<>();
-		
-		for(Ref<AnswerSum> refAnswerSum : answerSumRefList) {
+
+		for (Ref<AnswerSum> refAnswerSum : answerSumRefList) {
 			AnswerSum answerSum = refAnswerSum.get();
 
-			if(answerSum == null) {
+			if (answerSum == null) {
 				String[] s = new String[1];
-				s[0]="null";
+				s[0] = "null";
 				list.add(s);
-			}else {
+			} else {
 				String[] s = new String[7];
-				s[0]=answerSum.getId().toString();
-				s[1]=answerSum.getRefToi().toString();
-				s[2]=answerSum.getRefToi().get().getId().toString();
-				s[3]=answerSum.getMemberId();
-				Member member = null;
-				if(answerSum.getRefMember()!=null) {
-					member=answerSum.getRefMember().get();
+				s[0] = answerSum.getId().toString();
+				s[1] = answerSum.getRefToi().toString();
+				s[2] = answerSum.getRefToi().get().getId().toString();
+				s[3] = answerSum.getMemberId();
+
+//				Member member = null;
+//				if (answerSum.getRefMember() != null) {
+//					member = answerSum.getRefMember().get();
+//				}
+				Optional<Ref<Member>> refMem = Optional.ofNullable(answerSum.getRefMember());
+				Optional<Member> mem = refMem.map(rm -> rm.get());
+
+//				if (member == null) {
+				if(!mem.isPresent()) {
+					s[5] = "null";
+					s[4] = "null";
+				} else {
+					Member member = mem.get();
+					s[4] = member.toString();
+//					List<AnswerSum> answerSumList = member.getAnswerSumList();
+					List<Ref<AnswerSum>> refAnswerSumList = member.getRefAnswerSumList();
+
+					s[5] = refAnswerSumList.contains(Ref.create(new Key(AnswerSum.class,answerSum.getId()))) + "";
 				}
-				
-				if(member==null) {
-					s[5]="null";
-					s[4]="null";
-				}else {
-					s[4]=member.toString();
-					List<AnswerSum> answerSumList = member.getAnswerSumList();
-					s[5]=answerSumList.contains(answerSum)+"";
-				}
-				s[6]=answerSum.getDateString(answerSum.getAnswered());
+				s[6] = AnswerSum.getDateString(answerSum.getAnswered());
 				list.add(s);
-				
+
 			}
-					
+
 		}
-		
+
 		request.setAttribute("list", list);
 		request.setAttribute("t", t);
-		
+
 		request.getRequestDispatcher("/jsp/checkToiMember.jsp").forward(request, response);
 	}
 
