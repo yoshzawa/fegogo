738e5b6aa3c5cf4a76002daf39d0e31f33892734
yoshzawa <yoshzawa@gmail.com>
2020-03-16 16:02:53 +0900

    Exam2を作成

---
+++ src/main/java/jp/ac/jc21/t/yoshizawa/ver2/Exam2NoLogListServlet.java
@@ -0,0 +1,43 @@
+package jp.ac.jc21.t.yoshizawa.ver2;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jp.ac.jc21.t.yoshizawa.objectify.Exam;
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns = { "/exam2/Nolog/list" })
+public class Exam2NoLogListServlet extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+		Map<Long, Exam> examMap = Exam.loadAll();
+
+		List<String[]> datas = new ArrayList<String[]>();
+		for (Long k : examMap.keySet()) {
+			Exam e = examMap.get(k);
+			String[] s = new String[2];
+
+			s[0] = "<a href='/toi2/list?parentId=" + e.getId() + "'>" + e.getName() + "</a>";
+			s[1] = e.getToiRefListSize() + "";
+			datas.add(s);
+		}
+		
+		request.setAttribute("datas", datas);
+
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp2/nolog/examList.jsp");
+		rd.forward(request, response);
+
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/jsp2/nolog/examList.jsp
@@ -0,0 +1,63 @@
+<%@page import="java.util.List"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+
+<body>
+	<%
+		List<String[]> datas = (List<String[]>) request.getAttribute("datas");
+	%>
+
+	<nav aria-label="breadcrumb">
+		<ol class="breadcrumb">
+			<li class="breadcrumb-item"><a href="/">ホーム</a></li>
+			<li class="breadcrumb-item active" aria-current="page">試験 一覧</li>
+		</ol>
+	</nav>
+
+	<%@ include file="../common/header.jsp"%><br>
+	
+	<h1>登録されている試験の一覧</h1>
+
+	<%
+		if (datas == null || datas.size() == 0) {
+	%>
+	試験が登録されていません
+	<%
+		} else {
+	%>
+
+	<TABLE border="1" class="table table-striped table-hover">
+		<thead class="thead-dark">
+			<tr>
+				<TH>試験名</TH>
+				<TH>問題登録</TH>
+			</TR>
+		</thead>
+		<%
+			for (String[] s : datas) {
+		%>
+		<tr>
+			<td><%=s[0]%></td>
+			<td><%=s[1]%></td>
+		</tr>
+		<%
+			}
+		%>
+	</TABLE>
+
+
+	<%
+		}
+	%>
+
+
+</body>
+<%@ include file="../common/footer.jsp"%>
+
+</html>
