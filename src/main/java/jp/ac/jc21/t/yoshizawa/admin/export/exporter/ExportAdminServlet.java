4ac13cb5f459112c83b2cbd1a47fdc47060107f1
yoshzawa <yoshzawa@gmail.com>
2020-06-17 09:41:10 +0900

    古いEXPORTERを消したり、EXPORTERが正しくつながらないのを直したりした

---
+++ src/main/java/jp/ac/jc21/t/yoshizawa/admin/export/exporter/ExportAdminServlet.java
@@ -0,0 +1,35 @@
+package jp.ac.jc21.t.yoshizawa.admin.export.exporter;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ExportAdminServlet
+ */
+@WebServlet("/admin/exporter")
+public class ExportAdminServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ExportAdminServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/jsp/exportAdmin.jsp");
+		rd.forward(request, response);
+	}
+
+}
--- src/main/webapp/jsp/exportAdmin.jsp
+++ src/main/webapp/jsp/exportAdmin.jsp
@@ -19,15 +19,6 @@
 	<%@ include file="common/headerAdmin.jsp"%><br>
 	
 <H1>EXPORT一覧</H1>
-<H2>バージョン１：平成と同じ形式でEXPORT</H2>
-      <a class="disabled" href="/admin/answerSum/dumpAnswerSummary.csv">解答情報をCSVでダウンロード</a>
-	  <a class="disabled" href="/admin/answer/dumpAnswer.csv">解答詳細をCSVでダウンロード（TIMEOUT）</a>
-
-<H2>バージョン２：平成と同じ形式で、TASKでGoogleCloudConsoleに一度出力するタイプ</H2>
-
-      <a class="active" href="/admin/makeCsvTask">EXPORT生成</a>
-      <a class="active" href="/admin/dumpAnswerSummary2.csv">解答情報CSV</a>
-      <a class="active" href="/admin/dumpAnswer2.csv">解答詳細CSV</a>
 
 <H2>バージョン３：テーブルの中身をそのままexportするタイプ</H2>
       <a class="active" href="/admin/export/exam.csv">exam.csv</a>
