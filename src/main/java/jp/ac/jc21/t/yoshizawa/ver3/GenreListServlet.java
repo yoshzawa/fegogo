f846d52f824232428cf5d321c69549e3443fc01f
yoshzawa <yoshzawa@gmail.com>
2020-08-05 19:24:59 +0900

    V3Genre作りかけ

---
+++ src/main/java/jp/ac/jc21/t/yoshizawa/ver3/GenreListServlet.java
@@ -0,0 +1,69 @@
+package jp.ac.jc21.t.yoshizawa.ver3;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.TreeMap;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import jp.ac.jc21.t.yoshizawa.objectify.Exam;
+import jp.ac.jc21.t.yoshizawa.objectify.Toi;
+
+/**
+ * Servlet implementation class IndexServlet
+ */
+
+@WebServlet(urlPatterns = { "/ver3/genre/list" })
+public class GenreListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public GenreListServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		Map<Long, Exam> examMap = Exam.loadAll();
+		request.setAttribute("examMap", examMap);
+		
+		Optional<String> optExamId = Optional.ofNullable(request.getParameter("examId"));
+		
+		Long examId = null;
+		if(optExamId.isPresent()){
+			examId=Long.parseLong(optExamId.get());
+		}else{
+			Long maxKey = Collections.max(examMap.keySet());
+			examId=examMap.get(maxKey).getId();
+		}
+		request.setAttribute("examId", examId);
+		
+		Map<Long, Toi> toiMap = Exam.getById(examId).getToiMap();
+		request.setAttribute("toiMap", toiMap);
+		
+
+		
+		RequestDispatcher rd;
+			rd = request.getRequestDispatcher("/WEB-INF/jsp3/examList.jsp");
+
+		rd.forward(request, response);
+
+	}
+
+}
