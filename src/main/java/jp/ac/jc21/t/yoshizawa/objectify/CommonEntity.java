f89495815deb46f79fcb6e7886c1624be56948e4
yoshzawa <yoshzawa@gmail.com>
2021-08-15 17:56:39 +0900

    ver.4004に変更

--- src/main/java/jp/ac/jc21/t/yoshizawa/objectify/CommonEntity.java
+++ src/main/java/jp/ac/jc21/t/yoshizawa/objectify/CommonEntity.java
@@ -48,5 +48,5 @@ public class CommonEntity {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm");
 		return sdf.format(date);
 	}
-	final public static String ofyVersion = "4.00.1";
+	final public static String ofyVersion = "4.00.3";
 }
