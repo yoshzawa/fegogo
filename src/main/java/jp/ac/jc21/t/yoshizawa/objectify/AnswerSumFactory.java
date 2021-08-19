725e2a8f5b5cb0e61f420d679f3a44b5657773a5
yoshzawa <yoshzawa@gmail.com>
2021-08-19 11:24:06 +0900

    AnswerSumをMemberIdでソートできるように

--- src/main/java/jp/ac/jc21/t/yoshizawa/objectify/AnswerSumFactory.java
+++ src/main/java/jp/ac/jc21/t/yoshizawa/objectify/AnswerSumFactory.java
@@ -6,6 +6,7 @@ package jp.ac.jc21.t.yoshizawa.objectify;
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -19,8 +20,9 @@ import com.googlecode.objectify.Ref;
  *
  */
 public class AnswerSumFactory extends CommonEntity {
-	
-	public static final AnswerSum createAnswerSum(String name,Toi Toi,int noOfSeikai,Map<String,Ref<Answer>> mapRefAnswer) {
+
+	public static final AnswerSum createAnswerSum(String name, Toi Toi, int noOfSeikai,
+			Map<String, Ref<Answer>> mapRefAnswer) {
 		AnswerSum as = new AnswerSum();
 		as.setName(name);
 		as.setToiId(Toi.getId());
@@ -29,25 +31,24 @@ public class AnswerSumFactory extends CommonEntity {
 		as.setVersion(ofyVersion);
 		return as;
 	}
-	
+
 	public static final AnswerSum getById(long id) {
-		return (AnswerSum) getById(AnswerSum.class,id);
+		return (AnswerSum) getById(AnswerSum.class, id);
 	}
-	
+
 	public static final Optional<AnswerSum> getOptById(long id) {
 		return Optional.ofNullable(getById(id));
 	}
 
-	
 	@SuppressWarnings("unchecked")
 	public static final List<AnswerSum> loadAll() {
 		return (List<AnswerSum>) loadAll(AnswerSum.class);
 	}
-	
+
 	public static final List<AnswerSum> loadAll2() {
 		List<Member> list = Member.loadAll();
 		List<AnswerSum> allList = new ArrayList<AnswerSum>();
-		for(Member m:list) {
+		for (Member m : list) {
 			List<AnswerSum> asList = AnswerSum.getListByEMail(m.geteMail());
 			allList.addAll(asList);
 		}
@@ -55,16 +56,35 @@ public class AnswerSumFactory extends CommonEntity {
 	}
 
 	public final static List<AnswerSum> getListByEMail(String eMail) {
-		List<AnswerSum> list=null;
-			 list = (List<AnswerSum>)loadByIndex(AnswerSum.class,"name",eMail);
+		List<AnswerSum> list = null;
+		list = (List<AnswerSum>) loadByIndex(AnswerSum.class, "name", eMail);
 		return list;
 	}
 
-	
 	public static final List<AnswerSum> getListByToiId(Long toiId) {
 		List<AnswerSum> list;
-			list = ofy().load().type(AnswerSum.class).filter("toiId", toiId).list();
+		list = ofy().load().type(AnswerSum.class).filter("toiId", toiId).list();
+		list = sortByMemberId(list);
 
 		return list;
+	}
+
+	private static List<AnswerSum> sortByMemberId(List<AnswerSum> list) {
+		list.sort(new Comparator<AnswerSum>() {
+
+			@Override
+			public int compare(AnswerSum as1, AnswerSum as2) {
+				String memberId1 = as1.getMemberId();
+				String memberId2 = as2.getMemberId();
+				if (memberId1.equals(memberId2)) {
+					return (int) (as1.getAnswered().getTime() - as2.getAnswered().getTime());
+				} else {
+					return memberId1.compareTo(memberId2);
+				}
+			}
+
+		});
+		return list;
+
 	}
 }
---
+++ src/main/java/jp/ac/jc21/t/yoshizawa/servlet/admin/answerSum/AnswerSumListAdminByToiServlet.java
@@ -0,0 +1,35 @@
+package jp.ac.jc21.t.yoshizawa.servlet.admin.answerSum;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jp.ac.jc21.t.yoshizawa.objectify.*;
+
+@SuppressWarnings("serial")
+
+@WebServlet(urlPatterns = { "/admin/answerSum/listByToi" })
+public class AnswerSumListAdminByToiServlet extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+		long toiId = Long.parseLong(request.getParameter("toiId"));
+		String parentId = request.getParameter("parentId");
+		
+		List<AnswerSum> answerSumList = AnswerSum.getListByToiId(toiId);
+		request.setAttribute("answerSumList", answerSumList);
+		
+		request.setAttribute("redirectTo", "/admin/toi/list?parentId="+ parentId );
+
+		RequestDispatcher rd = request.getRequestDispatcher("/jsp/answerSumListAllAdmin.jsp");
+		rd.forward(request, response);
+
+	}
+}
